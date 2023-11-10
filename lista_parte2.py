b1a7ccd9e717b01e6a1956e8e750e3b420637e23
BrianDuarte <braianduarte199918@gmail.com>
2023-11-10 11:38:37 -0300

    Mas ejercicios de listas culminados

--- lista_parte2.py
+++ lista_parte2.py
@@ -83,6 +83,132 @@ minimo = min(numeros)
 print(f"El Maximo es {maximo} y el Minimo es {minimo}")
 
 
+#Duplicar valores: Dada una lista de números, crea una nueva lista que contenga cada número duplicado
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
+
+numeros_duplicados = []
+
+for i in numeros:
+
+    numeros_duplicados.append(i)
+    numeros_duplicados.append(i)
+
+print(numeros_duplicados)
+
+#Eliminar elementos impares: Elimina todos los elementos impares de una lista de números.
+
+numeros1 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38, 41, 44, 47, 50, 53, 56, 59]
+
+numeros_pares = []
+
+for i in numeros1:
+
+    if i % 2==0:
+
+        numeros_pares.append(i)
+
+print(numeros_pares)
+
+#Multiplicación de elementos: Dada una lista de números, crea una nueva lista que contenga cada número multiplicado por un valor específico.
+
+numeros3 = [10, 23, 7, 45, 12, 88, 34, 56, 2, 19, 5, 67, 3, 27, 50, 8, 61, 14, 32, 76, 9, 42, 68, 1, 77, 51, 28, 6, 94, 17]
+
+numeros_multiplicados = []
+
+for k in numeros3:
+
+    multiplicacion = k * 5
+
+    numeros_multiplicados.append(multiplicacion)
+
+print(numeros_multiplicados)   
+
+#Lista de cuadrados: Dada una lista de números, crea una nueva lista que contenga los cuadrados de los números.
+
+numeros4 = [42, 15, 7, 88, 12, 19, 32, 60, 25, 53, 6, 11, 99, 36, 29, 18, 51, 77, 21, 5, 63, 2, 8, 44, 1, 67, 40, 3, 54, 72, 48, 9, 64, 17, 35, 55, 22, 14, 70, 20, 58, 16, 23, 26, 13, 38, 49, 30, 41, 10, 47, 68, 4, 31, 50, 24, 61, 76, 33, 27, 52, 39, 62, 91, 43, 80, 37, 65, 46, 75, 83, 59, 45, 85]
+
+numeros_al_cuadrado = []
+for cuadrado in numeros4:
+
+     numero_cuadrado = cuadrado ** 2
+
+     numeros_al_cuadrado.append(numero_cuadrado)
+
+print(numeros_al_cuadrado)     
+
+#Promedio de elementos pares: Calcula el promedio de todos los elementos pares en una lista de números.
+
+numeros5 = [5, 8, 12, 2, 11, 14, 7, 9, 6, 1, 13, 4, 10, 3, 15]
+
+promedio = sum(numeros5) / len(numeros5) 
+
+print(f"El promedio es {promedio}")
+
+#Otra forma de hacerlo
+
+suma_numero_lista = 0
+contador = 0
+
+for i in numeros5:
+
+    suma_numero_lista+=i
+    contador +=1
+
+promedio2 = suma_numero_lista / contador
 
 
-    
+
+print(f"El promedio dos es {promedio2}")
+
+#Mayor diferencia: Encuentra la mayor diferencia entre dos elementos en una lista de números.
+
+lista_numeros = [10, 5, 8, 3, 12, 7, 15, 20, 18, 25]
+
+mayor_diferencia = max(lista_numeros) - min(lista_numeros)
+
+print(mayor_diferencia)
+
+
+#Suma acumulativa: Crea una nueva lista que contenga la suma acumulativa de los elementos de una lista dada.
+
+numero_lista = [10, 5, 8, 3, 12, 7, 15, 20, 18, 25]
+
+suma_acumulativa = []
+
+suma_temporal = 0
+
+for k in numero_lista:
+
+    suma_temporal +=k
+    suma_acumulativa.append(suma_temporal)
+
+print(suma_acumulativa)
+
+#Ordenar lista de cadenas: Dada una lista de cadenas, ordénalas alfabéticamente.
+
+lista_cadenas = ["manzana", "banana", "uva", "kiwi", "pera", "naranja", "fresa", "mango", "cereza", "piña"]
+
+
+lista_ordenada = sorted(lista_cadenas)
+
+print(lista_ordenada)
+
+
+#Lista de elementos únicos: Dada una lista con elementos repetidos, crea una nueva lista que contenga solo los elementos únicos en el mismo orden en que aparecen.
+
+lista_repetida = [1, 2, 2, 3, 4, 4, 5, 1, 6, 7, 8, 9, 9, 10]
+
+lista_sin_repetir = list(set(lista_repetida))
+
+print(lista_sin_repetir)
+
+
+
+
+
+
+
+
+
+
